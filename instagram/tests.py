f38b321f6427f9b99766459f268c861874aa66ed
jacqueline ngabo <ngabo.jay@gmail.com>
2022-06-09 12:04:36 +0300

    add changes

--- instagram/tests.py
+++ instagram/tests.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import *
 
 class TestProfileCase(TestCase):
   def setUp(self):
-    self.user = User(username='collo')
+    self.user = User(username='Geejay')
     self.user.save()
     
     self.profile = Profile(id=1, photo='test.jpg',bio='test stuff',user=self.user)
